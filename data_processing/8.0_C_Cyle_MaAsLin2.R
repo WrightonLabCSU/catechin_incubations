d35f102cb694fa8df304549c6711b2cf7066a637
Bridget McGivern <95941779+bmcgivern13@users.noreply.github.com>
2024-10-22 14:54:58 -0500

    Create 8.0_C_Cyle_MaAsLin2.R

---
+++ data_processing/8.0_C_Cyle_MaAsLin2.R
@@ -0,0 +1,116 @@
+library(readxl)
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+library(purrr)
+library(Maaslin2)
+
+
+paths=read_excel("Supplementary_Data_3.xlsx",sheet="carbon_cycle_pathways")
+
+##################
+## MaAslin2 = day 7
+##################
+# features as columns and samples as rows.
+wide_fx_sums=paths%>%
+  filter(time==7)%>%
+  select(-time,-treat)%>%
+  spread(key=path,value=sum,fill=0)
+wide_fx_sums=as.data.frame(wide_fx_sums)
+row.names(wide_fx_sums)=wide_fx_sums[,1]
+wide_fx_sums=wide_fx_sums[,-1]
+
+#Metadata file
+#Formatted with features as columns and samples as rows.
+meta=metadata%>%
+  filter(time==7)%>%
+  filter(treat!="CT")
+meta=as.data.frame(meta)
+row.names(meta)=meta$sample
+
+Maaslin2(input_data = wide_fx_sums,
+         input_metadata = meta,
+         output="paths_day07_maaslin2",
+         fixed_effects = c("treat"),
+         plot_heatmap = FALSE,
+         plot_scatter = FALSE)
+##################
+## MaAslin2 = day 14
+##################
+# features as columns and samples as rows.
+wide_fx_sums=paths%>%
+  filter(time==14)%>%
+  select(-time,-treat)%>%
+  spread(key=path,value=sum,fill=0)
+wide_fx_sums=as.data.frame(wide_fx_sums)
+row.names(wide_fx_sums)=wide_fx_sums[,1]
+wide_fx_sums=wide_fx_sums[,-1]
+
+#Metadata file
+#Formatted with features as columns and samples as rows.
+meta=metadata%>%
+  filter(time==14)%>%
+  filter(treat!="CT")
+meta=as.data.frame(meta)
+row.names(meta)=meta$sample
+
+Maaslin2(input_data = wide_fx_sums,
+         input_metadata = meta,
+         output="paths_day14_maaslin2",
+         fixed_effects = c("treat"),
+         plot_heatmap = FALSE,
+         plot_scatter = FALSE)
+
+##################
+## MaAslin2 = day 21
+##################
+# features as columns and samples as rows.
+wide_fx_sums=paths%>%
+  filter(time==21)%>%
+  select(-time,-treat)%>%
+  spread(key=path,value=sum,fill=0)
+wide_fx_sums=as.data.frame(wide_fx_sums)
+row.names(wide_fx_sums)=wide_fx_sums[,1]
+wide_fx_sums=wide_fx_sums[,-1]
+
+#Metadata file
+#Formatted with features as columns and samples as rows.
+meta=metadata%>%
+  filter(time==21)%>%
+  filter(treat!="CT")
+meta=as.data.frame(meta)
+row.names(meta)=meta$sample
+
+Maaslin2(input_data = wide_fx_sums,
+         input_metadata = meta,
+         output="paths_day21_maaslin2",
+         fixed_effects = c("treat"),
+         plot_heatmap = FALSE,
+         plot_scatter = FALSE)
+
+##################
+## MaAslin2 = day 35
+##################
+# features as columns and samples as rows.
+wide_fx_sums=paths%>%
+  filter(time==35)%>%
+  select(-time,-treat)%>%
+  spread(key=path,value=sum,fill=0)
+wide_fx_sums=as.data.frame(wide_fx_sums)
+row.names(wide_fx_sums)=wide_fx_sums[,1]
+wide_fx_sums=wide_fx_sums[,-1]
+
+#Metadata file
+#Formatted with features as columns and samples as rows.
+meta=metadata%>%
+  filter(time==35)%>%
+  filter(treat!="CT")
+meta=as.data.frame(meta)
+row.names(meta)=meta$sample
+
+Maaslin2(input_data = wide_fx_sums,
+         input_metadata = meta,
+         output="paths_day35_maaslin2",
+         fixed_effects = c("treat"),
+         plot_heatmap = FALSE,
+         plot_scatter = FALSE)
